3cab51701583b9136cd3d8fd78adb7f9b023c181
Melvin <31448155+melvio@users.noreply.github.com>
2021-08-10 08:32:21 +0200

    let basics

---
+++ snippets/integers/let-basics.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+
+# let evaluates the part on the right of the = as an expression
+let i=1+1
+echo "${i}"
+# 2
+
+# watch out: let does does not set the integer attribute on the variable
+let j=1+1
+declare -p j # declare -- j="2"
+
+# j is evaluated as a string so "1" is appended
+j+=1
+echo ${j} # 21
+
+set -o xtrace
+b=2
+c=4
+# watch out: '*' is the globbing operator here
+let a=b * c # This will evaluate to:  let a=b declaration.sh let-basics.sh {more files} c
+echo "${a}" # 2     (since only a=b was evaluated before the syntax error occurred)
+
+# this does work (somehow now the * is not evaluated as a globbing operator)
+let a=b*c
+echo "${a}" # 8
+
